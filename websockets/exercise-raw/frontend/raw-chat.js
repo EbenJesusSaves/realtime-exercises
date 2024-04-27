7e004072dab449f6fe09b732af4eab693a3bde0c
Eben <96250954+EbenJesusSaves@users.noreply.github.com>
2024-04-27 15:44:30 +0000

    added websocket raw

--- websockets/exercise-raw/frontend/raw-chat.js
+++ websockets/exercise-raw/frontend/raw-chat.js
@@ -11,13 +11,19 @@ chat.addEventListener("submit", function (e) {
 });
 
 async function postNewMsg(user, text) {
-  // code goes here
+  const data = { user, text };
+  ws.send(JSON.stringify(data));
 }
 
 const ws = new WebSocket("ws://localhost:8080", ["json"]);
 // do this when you open a websocket connection
 ws.addEventListener("open", () => {
   presence.innerText = "🍏";
+});
+ws.addEventListener("message", (event) => {
+  const data = JSON.parse(event.data);
+  allChat = data.msg;
+  render();
 });
 
 function render() {
